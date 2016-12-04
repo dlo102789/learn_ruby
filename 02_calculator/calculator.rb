4022b2ff419abc747a224ad027adb1d77cb9b868
Derek Lo <dlo102789@gmail.com>
2016-12-04 00:19:33 -0600

    Completed up to assignment 03-Simon Says

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,39 @@
 #write your code here
+def add (first, second)
+	first + second
+end
+
+def subtract (first, second)
+	first - second
+end
+
+def sum (ar)
+	fnum = 0
+	ar.each do |num|
+		fnum += num
+	end
+	fnum
+end
+
+def multiply (first, *rest)
+	while rest.length > 0
+		first *= rest.pop
+	end
+	first
+end
+
+def power (first, second)
+	temp = 1
+	second.times do
+		temp *= first
+	end
+	temp
+end
+
+def factorial(number)
+	if number <= 1
+		return 1
+	end
+
+	number * factorial(number-1)
+end
--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,42 @@
 #write your code here
+def echo (phrase)
+	phrase
+end
+
+def shout(phrase)
+	phrase.upcase
+end
+
+def repeat(word, k=2)
+	phrase = word
+	k -= 1
+	k.times do
+		phrase += ' ' + word
+	end
+	phrase
+end
+
+def start_of_word (word,num)
+	array = word.split("")
+	array = array.shift(num)
+	word = array.join
+end
+
+def first_word (phrase)
+	array = phrase.split(" ")
+	array[0]
+end
+
+def titleize (p)
+	array = p.split()
+	array.each do |item|
+		if (item != 'the' and item != 'and' and item != 'over')
+			item.capitalize!
+		end
+	end
+	array[0].capitalize!
+	if array.length == 1
+		return array[0]
+	end
+	array.join(" ")
+end
